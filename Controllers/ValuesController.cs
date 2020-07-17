3b8e88208267465b93466eb4983502c25215f767
Deepanshu Arora <deepanshuarora.098@gmail.com>
2020-07-17 10:18:55 +0530

    Change Added

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzureWebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2" , "Values3" };
         }
 
         // GET api/values/5
